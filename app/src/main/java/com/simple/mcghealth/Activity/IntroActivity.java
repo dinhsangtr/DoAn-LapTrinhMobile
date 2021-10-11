d9b78096f636f85044e264b6b146f842a47e76f9
D.S <sangtruong2811@gmail.com>
2021-10-11 15:04:25 +0700

    now

---
+++ app/src/main/java/com/simple/mcghealth/Activity/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.simple.mcghealth.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.simple.mcghealth.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
---
+++ app/src/main/java/com/simple/mcghealth/Activity/IntroFTActivity.java
@@ -0,0 +1,16 @@
+package com.simple.mcghealth.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.simple.mcghealth.R;
+
+public class IntroFTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_ftactivity);
+    }
+}
